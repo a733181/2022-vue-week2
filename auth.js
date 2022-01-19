7b3e25bca15ad293f8ec64d77be79d525a71049f
a733181 <a733181@gmail.com>
2022-01-19 14:35:11 +0800

    first commit

---
+++ auth.js
@@ -0,0 +1,45 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      email: {
+        isValue: false,
+        value: '',
+      },
+      password: {
+        isValue: false,
+        value: '',
+      },
+      token: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      if (this.email.value === '' || !this.email.value.includes('@')) {
+        this.email.isValue = true;
+        return;
+      }
+      if (this.password.value === '') {
+        this.password.isValue = true;
+        return;
+      }
+
+      axios
+        .post('https://vue3-course-api.hexschool.io/v2/admin/signin', {
+          username: this.email.value,
+          password: this.password.value,
+        })
+        .then((res) => {
+          // console.log(res.data);
+          this.token = res.data.token;
+          const expired = new Date(res.data.expired).toUTCString();
+          document.cookie = `token=${this.token};expires=${expired};`;
+          window.location = 'products.html';
+        })
+        .catch((error) => {
+          alert(error.response.data);
+        });
+    },
+  },
+});
+
+app.mount('#app');
---
+++ products.js
@@ -0,0 +1,50 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      url: 'https://vue3-course-api.hexschool.io/v2/api/',
+      products: '',
+      index: null,
+    };
+  },
+  computed: {
+    productDetail() {
+      const products = this.products[this.index];
+      return products;
+    },
+  },
+  methods: {
+    checkLogin() {
+      axios
+        .post(`${this.url}user/check`)
+        .then(() => {})
+        .catch(() => {
+          window.location = 'index.html';
+        });
+    },
+    getProductsData() {
+      axios
+        .get(`${this.url}tita/admin/products/all`)
+        .then((res) => {
+          const { products } = res.data;
+          this.products = products;
+        })
+        .catch((error) => {
+          alert(error.response.data);
+        });
+    },
+    productView(index) {
+      this.index = index;
+    },
+  },
+  created() {
+    const cookieToken = document.cookie.replace(
+      /(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/,
+      '$1'
+    );
+    axios.defaults.headers.common.Authorization = cookieToken;
+    this.checkLogin();
+    this.getProductsData();
+  },
+});
+
+app.mount('#app');
